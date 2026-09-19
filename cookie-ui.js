5ace44782aa906ab6ca7b729b9b25bf45b3f7625
kuf11 <romann898989@gmail.com>
2026-09-19 21:04:23 +0500

    rollback: restore project state from 2026-09-18

---
+++ cookie-ui.js
@@ -0,0 +1,1 @@
+(()=>{document.querySelectorAll('.lf-global-loader').forEach(node=>node.remove());if(location.pathname.startsWith('/miniapp'))document.querySelectorAll('.essential-cookie-notice').forEach(node=>node.remove())})();
